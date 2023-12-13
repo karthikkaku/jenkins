c28fb3e6cbf838080e828ce508d2f83866108228
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2023-12-13 20:46:12 +0530

    Update create_ami.ps1

--- create_ami.ps1
+++ create_ami.ps1
@@ -1,20 +1,3 @@
-# Function to check if AWS CLI is installed
-function Check-AWSCLIInstallation {
-    return (Test-Path (Join-Path $env:ProgramFiles 'Amazon\AWSCLI\aws.exe'))
-}
-
-# Function to install AWS CLI
-function Install-AWSCLI {
-    Write-Output "AWS CLI not found. Installing AWS CLI..."
-
-    # Download AWS CLI installer
-    $awsCLIInstaller = "$env:TEMP\AWSCLIV2.exe"
-    Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.exe" -OutFile $awsCLIInstaller
-
-    # Run AWS CLI installer silently
-    Start-Process -Wait -FilePath $awsCLIInstaller -ArgumentList "/S" -NoNewWindow
-}
-
 # Check if AWS CLI is available
 $awsExecutable = "C:\Program Files\Amazon\AWSCLI\aws.exe"
 if (-not (Test-Path $awsExecutable)) {
@@ -33,45 +16,30 @@ $InstanceID = $args[0]
 $BaseAMIName = $args[1]
 $Description = $args[2]
 
-Write-Output "Instance ID: $InstanceID"
-Write-Output "BaseAMIName: $BaseAMIName"
-Write-Output "Description: $Description"
-
-# Set your Slack API token here
-$SLACK_API_TOKEN = "xoxb-6304431362048-6320659208197-YqD4S8FA2leoPaceMnKOEM2m"
-
 # Generate a unique timestamp
 $Timestamp = Get-Date -Format "yyyyMMddHHmmss"
 
 # Create a unique AMI name by appending the timestamp to the base AMI name
 $AMIName = "${BaseAMIName}_${Timestamp}"
 
-# Rest of your script to handle AMI creation and Slack notification
-# Check if an AMI with the specified name already exists
-$existingAmi = aws ec2 describe-images --owners self --filters "Name=name,Values=$AMIName" --query 'Images[*].ImageId' --output text
+# Rest of your AMI creation code
+# ...
 
-if ($existingAmi) {
-    Write-Output "An AMI with the name '$AMIName' already exists (AMI ID: $existingAmi). Please choose a different base AMI name."
-    exit 0
-}
-
-# Create an AMI from the specified EC2 instance
-$AMIId = aws ec2 create-image --instance-id $InstanceID --name $AMIName --description $Description --output text
+# Create an AMI from the specified EC2 instance using the AWS CLI
+$AMIId = & $awsExecutable ec2 create-image --instance-id $InstanceID --name $AMIName --description $Description --output text
 
 Write-Output "Creating AMI with ID: $AMIId and name: $AMIName"
 
-# Wait for the AMI creation to complete
-$amiStatus = "pending"
-while ($amiStatus -eq "pending") {
-    Start-Sleep -Seconds 30  # Wait for 30 seconds before checking again
-    $ami = aws ec2 describe-images --image-ids $AMIId --query 'Images[*].State' --output text
-    $amiStatus = $ami
-}
+# Wait for the AMI creation to complete using the AWS CLI
+# ...
 
+# Slack notification integration using AWS CLI
 if ($amiStatus -eq "available") {
     Write-Output "AMI creation completed. AMI ID: $AMIId"
     $text = "$AMIId AMI Created Successfully"
-    Invoke-RestMethod -Uri "https://hooks.slack.com/services/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" -Method Post -Body (@{text = $text} | ConvertTo-Json) -ContentType "application/json"
+
+    # Send a notification to Slack using AWS CLI
+    & $awsExecutable lambda invoke --function-name "slackNotificationLambdaFunction" --payload "{ \"text\": \"$text\" }" --region us-east-1
 } else {
     Write-Output "AMI creation failed or timed out."
 }
