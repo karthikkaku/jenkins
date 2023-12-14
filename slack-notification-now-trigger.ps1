5869a09cdf3fbcc51eef8914fe590baaeac057ea
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2023-12-14 14:09:31 +0530

    Update and rename slack-notification-now-trigger to slack-notification-now-trigger.ps1

---
+++ slack-notification-now-trigger.ps1
@@ -0,0 +1,16 @@
+ param (
+    [Parameter(Mandatory = $true)]
+    [string]$NotificationText,
+)
+
+if (-not [string]::IsNullOrEmpty($result)) {
+    # Prepare JSON payload
+    $body = @{
+        text = "$NotificationText"
+    } | ConvertTo-Json
+
+    # Send a POST request to the Slack webhook URL
+    Invoke-RestMethod -Uri "https://hooks.slack.com/services/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" -Method Post -ContentType "application/json" -Body $body
+} else {
+    Write-Output "The content of result.txt is empty or null."
+}
