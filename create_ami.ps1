6c142614633375b186f183e07ee823f134892edd
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2023-12-18 11:24:23 +0530

    Update create_ami.ps1

--- create_ami.ps1
+++ create_ami.ps1
@@ -58,7 +58,7 @@ while ($amiStatus -eq "pending") {
 
 if ($amiStatus -eq "available") {
     Write-Output "AMI creation completed. AMI ID: $AMIId"
-    $message = "AMI creation completed. AMI ID: $AMIId"
+    $message = "$AMIId"
     Add-Content -Path "result.txt" -Value $message
 } else {
     Write-Output "AMI creation failed or timed out."
